970329b137b1e29b8ec48633b91270be67153ad1
palakagrawal-palak <74971540+palakagrawal-palak@users.noreply.github.com>
2021-01-27 13:43:19 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,34 @@
+class Bob{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:1.2,
+			friction:0.4,
+			density:3
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+			rectMode(CENTER)
+			strokeWeight(2);
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint ;
+
+var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5;
+var rope1, rope2, rope3, rope4, rope5;
+var roofObject;
+
+function preload()
+{
+	
+}
+
+function setup() {
+
+	createCanvas(1300, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	bobObject1 = new Bob(300,350,50);
+	bobObject2 = new Bob(350,350,50);
+	bobObject3 = new Bob(400,350,50);
+	bobObject4 = new Bob(450,350,50);
+	bobObject5 = new Bob(500,350,50);
+	roofObject = new Roof(400,200,300,20);
+    rope1= new Rope(bobObject1.body,roofObject.body,-50*2,0);
+    rope2= new Rope(bobObject2.body,roofObject.body,-25*2,0);
+    rope3= new Rope(bobObject3.body,roofObject.body,-0*2,0);
+    rope4= new Rope(bobObject4.body,roofObject.body,25*2,0);
+    rope5= new Rope(bobObject5.body,roofObject.body,50*2,0);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  roofObject.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    Matter.Body.applyForce(bobObject1.body, bobObject1.body.position,{x:-100,y:-100})
+  }
+}
+
+
